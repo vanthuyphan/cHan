30a2360f98f5938205a186ef874d433471b1211c
Van Phan <vanthuyphan@gmail.com>
2017-07-15 21:49:13 +0700

    config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'shopbanhang');
+define('DB_NAME', 'hanle');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
