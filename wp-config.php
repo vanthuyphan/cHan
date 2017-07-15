0a6254b894323264b5ddaceb684eddd7da7a68c1
Van Phan <vanthuyphan@gmail.com>
2017-07-15 14:20:38 +0700

    hey

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'shopbanhang');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
